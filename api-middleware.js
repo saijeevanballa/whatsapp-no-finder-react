7be095520e4ffea33ca0b85474ae8d2f004576a2
saijeevanballa <saijeevanballa@gmail.com>
2020-06-02 21:52:08 +0530

    1 file modified

--- api-middleware.js
+++ api-middleware.js
@@ -1,2 +1,4 @@
-export const BASE_URL = "https://nw3h8.sse.codesandbox.io/v1/numbers/";
+export const BASE_URL = 
+"https://whatsfind-p95mf96p4.now.sh/v1/numbers";
+//"https://nw3h8.sse.codesandbox.io/v1/numbers/";
 
